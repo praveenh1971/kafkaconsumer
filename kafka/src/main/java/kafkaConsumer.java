0cfc38d35aa7767546757194d82d4047efdef67f
praveen hanumanthareddy <praveen_h@yahoo.com>
2022-01-22 16:22:38 -0500

    first cut

---
+++ kafka/src/main/java/kafkaConsumer.java
@@ -0,0 +1,90 @@
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.KTable;
+import org.apache.kafka.streams.kstream.Produced;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Locale;
+import java.util.Properties;
+import java.util.concurrent.CountDownLatch;
+
+public class kafkaConsumer {
+
+    public static void main(String[] args) {
+
+        final Properties props = new Properties();
+
+        props.putIfAbsent(StreamsConfig.APPLICATION_ID_CONFIG, "streams-wordcount");
+        props.putIfAbsent(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.putIfAbsent(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, 0);
+        props.putIfAbsent(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+        props.putIfAbsent(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+
+        // setting offset reset to earliest so that we can re-run the demo code with the same pre-loaded data
+        // Note: To re-run the demo, you need to use the offset reset tool:
+        // https://cwiki.apache.org/confluence/display/KAFKA/Kafka+Streams+Application+Reset+Tool
+        props.putIfAbsent(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        props.putIfAbsent(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "MyGrop22");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+
+
+        KafkaConsumer<String,String> consumer = new KafkaConsumer<String, String>(props);
+        consumer.subscribe(Arrays.asList("quickstart-events"));
+        ConsumerRecords<String, String> values= consumer.poll(Duration.ofMillis(1000));
+        values.forEach(a->{
+
+            System.out.println("k " +  a.key() + " " + a.value());
+
+        });
+
+        final CountDownLatch latch = new CountDownLatch(1);
+        startStream(props);
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static final String INPUT_TOPIC = "quickstart-events";
+    public static final String OUTPUT_TOPIC = "streams-wordcount-output";
+    static void createWordCountStream(final StreamsBuilder builder) {
+        final KStream<String, String> source = builder.stream(INPUT_TOPIC);
+
+        final KTable<String, Long> counts = source.filter((key, value)->{System.out.println("KEy " + key + " val" + value); return true;})
+                .flatMapValues(value -> Arrays.asList(value.toLowerCase(Locale.getDefault()).split("\\W+")))
+                .groupBy((key, value) -> value)
+                .count();
+
+        // need to override value serde to Long type
+        counts.toStream().to(OUTPUT_TOPIC, Produced.with(Serdes.String(), Serdes.Long()));
+    }
+    public  static  final void startStream( Properties props ){
+        final StreamsBuilder builder = new StreamsBuilder();
+        createWordCountStream(builder);
+        final KafkaStreams streams = new KafkaStreams(builder.build(), props);
+
+
+        try {
+            streams.start();
+
+        } catch (final Throwable e) {
+            e.printStackTrace();
+
+        }
+    }
+}
